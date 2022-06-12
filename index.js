228ddc323d9a04989e22d2284678800982a2d65e
+dorcus <+wayumulu58@gmail.com>
2022-06-12 21:27:57 +0300

    assignment commit

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+
+const findMatching = (drivers, string) => {
+  return drivers.filter((name) => name.toLowerCase() === string.toLowerCase());
+};
+
+const fuzzyMatch = (drivers, string) => {
+  return drivers.filter((index) => index[0] === string[0]);
+};
+
+const matchName = (drivers, string) => {
+  return drivers.filter((data) => data.name === string);
+};
